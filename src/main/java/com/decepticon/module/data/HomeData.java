3be0bba07e854d390bc00716339e743a6b82eb97
fauri.hakim <fauri.hakim@gdn-commerce.com>
2020-05-30 19:40:54 +0700

    adding data with lombok

---
+++ src/main/java/com/decepticon/module/data/HomeData.java
@@ -0,0 +1,10 @@
+package com.decepticon.module.data;
+
+import lombok.Data;
+
+@Data
+public class HomeData {
+    private String city;
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/decepticon/module/data/RoomData.java
@@ -0,0 +1,10 @@
+package com.decepticon.module.data;
+
+import lombok.Data;
+
+@Data
+public class RoomData {
+    private String firstName;
+    private String lastName;
+    private String email;
+}

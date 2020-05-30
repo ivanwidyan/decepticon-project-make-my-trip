c74ef11b3b0a54d6fc865f1dd4c63ceddc0065f9
Gokul Gunasekaran <gokul.gunasekaran@gdn-commerce.com>
2020-05-30 20:22:34 +0530

    slider changes

--- src/test/java/com/decepticon/module/ui/SearchPage.java
+++ src/test/java/com/decepticon/module/ui/SearchPage.java
@@ -1,13 +1,21 @@
 package com.decepticon.module.ui;
 
+import com.decepticon.module.constant.Consts;
 import com.decepticon.module.constant.ValueConsts;
+import com.decepticon.module.utils.UiUtility;
 import com.decepticon.module.utils.Utility;
+import cucumber.api.java.sl.In;
+import lombok.experimental.var;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -15,11 +23,11 @@ import static org.junit.Assert.assertEquals;
 
 public class SearchPage extends PageObject {
     Utility utility = new Utility();
-
+    UiUtility uiUtility = new UiUtility();
     //filter by user rating
     @FindBy(xpath = "//input[@id='search']")
     WebElementFacade searchBox;
-    private String filterByUserRating = "//div[@id=\"hlistpg_fr_user_rating\"]/ul/li";
+    private String filterByUserRating = "//label[contains(text(),'%s')]";
     @FindBy(xpath = "//p[@class=\"whiteText latoBlack font22\"]")
     WebElementFacade popUpOnSearchResult;
     @FindBy(className = "mmBackdrop wholeBlack")
@@ -41,6 +49,7 @@ public class SearchPage extends PageObject {
     //element locators to select the hotel
     @FindBy(xpath = "//p[@class='appendBottom15']")
     WebElementFacade moveToBottomOfThePage;
+    String loadingRequest="//p[@class='appendBottom15']";
     @FindBy(className = "appendTop20 appendBottom20 font22 latoBlack blackText textCenter")
     WebElementFacade endPageMessage;
     @FindBy(xpath = "//div[@class='listingRowOuter']/a")
@@ -59,58 +68,68 @@ public class SearchPage extends PageObject {
         }
         utility.scrollToElement(getDriver(), userRatingFilter);
         utility.waitTillPageLoads(getDriver());
-        List<WebElement> filterByUserRatings = getDriver().findElements(By.xpath(filterByUserRating));
-        for (int i = 0; i < filterByUserRatings.size(); i++) {
-            if (filterByUserRatings.get(i).getText().contains(userRating)) {
-                int value = i + 1;
-                getDriver().findElement(By.xpath(filterByUserRating + "[" + value + "]")).click();
-                break;
-            }
-        }
+        uiUtility.fromXpathtoWebElement(filterByUserRating,userRating).click();
         waitForAngularRequestsToFinish();
-        String appliedFilter = "";
-        for (WebElementFacade element : appliedFilters) {
-            if (element.getText().equalsIgnoreCase(userRating)) {
-                appliedFilter = element.getText();
-            }
-        }
-        assertEquals(userRating, appliedFilter);
     }
 
     public void filterByPrice(String price) {
         Actions actions = new Actions(getDriver());
-//        utility.scrollToElement(getDriver(), priceFilter);
-        waitForAngularRequestsToFinish();
         WebElement element = getDriver().findElement(By.xpath(priceSliderPath));
+        utility.scrollToElement(getDriver(), priceFilter);
         if (Integer.valueOf(price) > 0) {
-            Integer max = Integer.valueOf(maxPrice.getText().split(" ")[1]);
-            Integer min = Integer.valueOf(minPrice.getText().split(" ")[1]);
-            Integer maxPriceDivision = max / 500;
             Integer currentDivision = Integer.valueOf(price) / 500;
             for (int i = 0; i < currentDivision; i++) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 actions.clickAndHold(element).moveByOffset(3, 0).release().perform();
             }
         }
+    }
+    public void assertion(String price,String userRating)
+    {
         utility.waitTillPageLoads(getDriver());
         String appliedFilter = "";
-        for (WebElementFacade element1 : appliedFilters) {
-            if (element1.getText().startsWith(ValueConsts.INR)) {
-                String currentValue = element1.getText().split(" ")[1];
+        for (WebElementFacade element : appliedFilters)
+        {
+            if (element.getText().startsWith(ValueConsts.INR))
+            {
+                String currentValue = element.getText().split(" ")[1];
                 String minSelected = currentValue.split("-")[0];
                 String maxSelected = currentValue.split("-")[1];
-
                 if (minSelected.equalsIgnoreCase(price)) {
                     appliedFilter = minSelected;
                 }
+                assertEquals(price, appliedFilter);
+            }
+            if(element.getText().contains(userRating))
+            {
+                appliedFilter=element.getText();
+                assertEquals(userRating,appliedFilter);
+            }
+            appliedFilter="";
+        }
+    }
+    public void selectHotel()
+    {
+//        utility.scrollToElement(getDriver(),moveToBottomOfThePage);
+//        if(getDriver().findElements(By.className("appendTop20 appendBottom20 font22 latoBlack blackText textCenter")).size()!=0)
+//        {
+//            utility.scrollToElement(getDriver(),moveToBottomOfThePage);
+//        }
+        int i=0;
+        while (i==0)
+        {
+            if(endPageMessage.isVisible())
+            {
+                i=1;
+            }
+            else {
+                utility.scrollToElement(getDriver(), moveToBottomOfThePage);
             }
         }
-        assertEquals(price, appliedFilter);
     }
-}
 
-//Filter by price - working
-//Filter by star rating -done
-//Apply Filter - done
-//Search for 5th hotel - xpath done
-//Capture the name - xpath done
-//Clickon it
+}

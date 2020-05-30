7dc0a352383db42d6d4d9c27468ebdf943c42b6b
Gokul Gunasekaran <gokul.gunasekaran@gdn-commerce.com>
2020-05-30 18:53:11 +0530

    filter by price-slider

--- src/test/java/com/decepticon/module/ui/SearchPage.java
+++ src/test/java/com/decepticon/module/ui/SearchPage.java
@@ -2,6 +2,8 @@ package com.decepticon.module.ui;
 
 import com.decepticon.module.constant.Constants;
 import com.decepticon.module.utils.Utility;
+import cucumber.api.java.sl.In;
+import lombok.experimental.var;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
@@ -35,12 +37,12 @@ public class SearchPage extends PageObject {
     @FindBy(xpath = "//span[@class=\"minValue\"]")
     WebElementFacade minPrice;
     @FindBy(xpath = "//span[@class=\"maxValue\"]")
-    WebElementFacade maxValue;
+    WebElementFacade maxPrice;
 
     //filter by price
-    @FindBy(id="hlistpg_fr_price_per_night")
+    @FindBy(xpath = "//div[@id=\"hlistpg_fr_price_per_night\"]/div")
     WebElementFacade priceFilter;
-    private String priceSliderPath="//div[@id=\"hlistpg_fr_price_per_night\"]/div/div/div/span";
+    private String priceSliderPath = "//div[@id=\"hlistpg_fr_price_per_night\"]/div/div/div/span";
 
     //element locators to select the hotel
     @FindBy(xpath = "//p[@class='appendBottom15']")
@@ -55,8 +57,7 @@ public class SearchPage extends PageObject {
         openUrl("https://www.makemytrip.com/hotels/hotel-listing/?_uCurrency=INR&checkin=05312020&checkout=06012020&city=RGPUDD&country=IN&locusId=RGPUDD&locusType=region&roomStayQualifier=2e0e3e0e&searchText=Puducherry%2C%20India&visitorId=86609899-567a-479b-8c6e-5238de82f0c3");
     }
 
-    public void filterByUserRating(String userRating)
-    {
+    public void filterByUserRating(String userRating) {
         //filter by userRating
         if (popUpOnSearchResult.isVisible()) {
             getDriver().navigate().refresh();
@@ -65,36 +66,51 @@ public class SearchPage extends PageObject {
         utility.scrollToElement(getDriver(), userRatingFilter);
         utility.waitTillPageLoads(getDriver());
         List<WebElement> filterByUserRatings = getDriver().findElements(By.xpath(filterByUserRating));
-        for (int i=0;i<filterByUserRatings.size();i++)
-        {
-            if (filterByUserRatings.get(i).getText().contains(userRating))
-            {
-                int value=i+1;
-                getDriver().findElement(By.xpath(filterByUserRating+"["+value+"]")).click();
+        for (int i = 0; i < filterByUserRatings.size(); i++) {
+            if (filterByUserRatings.get(i).getText().contains(userRating)) {
+                int value = i + 1;
+                getDriver().findElement(By.xpath(filterByUserRating + "[" + value + "]")).click();
                 break;
             }
         }
         waitForAngularRequestsToFinish();
-        String appliedFilter="";
-        for (WebElementFacade element:appliedFilters)
-        {
-            if(element.getText().equalsIgnoreCase(userRating))
-            {
-                appliedFilter=element.getText();
+        String appliedFilter = "";
+        for (WebElementFacade element : appliedFilters) {
+            if (element.getText().equalsIgnoreCase(userRating)) {
+                appliedFilter = element.getText();
             }
         }
-        assertEquals(userRating,appliedFilter);
+        assertEquals(userRating, appliedFilter);
     }
-    public void filterByPrice(String price)
-    {
-        utility.scrollToElement(getDriver(),priceFilter);
-        String [] min=minPrice.getText().split(Constants.INR,1);
-        if(Integer.valueOf(price)<Integer.valueOf(min[1]))
-        {
 
-        }
+    public void filterByPrice(String price) {
         Actions actions = new Actions(getDriver());
-//        actions.dragAndDropBy()
+//        utility.scrollToElement(getDriver(), priceFilter);
+        waitForAngularRequestsToFinish();
+        WebElement element = getDriver().findElement(By.xpath(priceSliderPath));
+        if (Integer.valueOf(price) > 0) {
+            Integer max = Integer.valueOf(maxPrice.getText().split(" ")[1]);
+            Integer min = Integer.valueOf(minPrice.getText().split(" ")[1]);
+            Integer maxPriceDivision = max / 500;
+            Integer currentDivision = Integer.valueOf(price) / 500;
+            for (int i = 0; i < currentDivision; i++) {
+                actions.clickAndHold(element).moveByOffset(3, 0).release().perform();
+            }
+        }
+        utility.waitTillPageLoads(getDriver());
+        String appliedFilter = "";
+        for (WebElementFacade element1 : appliedFilters) {
+            if (element1.getText().startsWith(Constants.INR)) {
+                String currentValue = element1.getText().split(" ")[1];
+                String minSelected = currentValue.split("-")[0];
+                String maxSelected = currentValue.split("-")[1];
+
+                if (minSelected.equalsIgnoreCase(price)) {
+                    appliedFilter = minSelected;
+                }
+            }
+        }
+        assertEquals(price, appliedFilter);
     }
 }
 

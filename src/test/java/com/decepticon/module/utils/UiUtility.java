243e839ad640ef9cfe254af4a42375c4847d4e1d
william.halim <william.halim@gdn-commerce.com>
2020-05-30 21:07:45 +0700

    hotel detail page

--- src/test/java/com/decepticon/module/utils/UiUtility.java
+++ src/test/java/com/decepticon/module/utils/UiUtility.java
@@ -4,9 +4,20 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.openqa.selenium.By;
 
+import java.util.List;
+
 public class UiUtility extends PageObject {
 
     public WebElementFacade fromXpathtoWebElement(String xpath, String value){
         return find(By.xpath(String.format(xpath, value)));
+    }
+    public WebElementFacade fromXpathtoWebElementString(String xpath){
+        return find(By.xpath(String.format(xpath)));
+    }
+
+    public List<WebElementFacade> xpathToWebElement(String xpath) {
+        List<WebElementFacade> webElementFacades = findAll(new By.ByXPath(xpath));
+
+        return webElementFacades;
     }
 }

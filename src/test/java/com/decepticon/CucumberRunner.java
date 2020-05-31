b044d1f929d1b181b33f1fdf1d5f81973cefbfaa
william.halim <william.halim@gdn-commerce.com>
2020-05-31 14:07:36 +0700

    Merge branch 'develop' of github.com:ivanwidyan/decepticon-project into develop
    # Conflicts:
    #	src/test/resources/features/MakeMyTrip.feature

--- src/test/java/com/decepticon/CucumberRunner.java
+++ src/test/java/com/decepticon/CucumberRunner.java
@@ -9,6 +9,6 @@ import org.junit.runner.RunWith;
         junit = "--step-notifications",
         plugin = {"pretty", "json:target/cucumber.json"},
         features = "src/test/resources/features/",
-        tags = {"@SearchandHdetail"})
+        tags = {"@MakeMyTrip"})
 public class CucumberRunner {
 }
